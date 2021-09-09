3b8f0e8d9c2e9df15c39b3012b530bc4a346f941
Arafath Miah <miah.arafath@gmail.com>
2021-09-09 11:01:39 +0100

    feeling lucky button works

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -4,6 +4,9 @@ const searchBar = document.getElementById("search-bar");
 // Grabbing submit button
 const searchButton = document.getElementById("search-button");
 
+// Grabbing the feeling lucky button
+const luckyButton = document.getElementById("lucky-button");
+
 const updateInfo = (data) => {
     // Section where the results will be appended
     const searchSection = document.getElementById("search-results")
@@ -15,6 +18,7 @@ const updateInfo = (data) => {
 
     for (const element of data) {
         // Concatenating each element to a single string
+        // Lazy way to do, not recommended
         toAppend += `
 
         <div class="row mx-3">
@@ -53,7 +57,8 @@ const searchAndUpdate = (words) => {
         .then(data => updateInfo(data))
 };
 
-searchBar.addEventListener("keypress", (e) => {
+// Search after pressing the Enter key from the keyboard
+searchBar.addEventListener("keypress", e => {
     e.preventDefault();
 
     if (e.key == "Enter") {
@@ -67,11 +72,39 @@ searchBar.addEventListener("keypress", (e) => {
     }
 });
 
-searchButton.addEventListener("click", (e) => {
+// Search after clicking the search button
+searchButton.addEventListener("click", e => {
     e.preventDefault();
 
+    // Won't process empty search query
     if (searchBar.value == "undefined")
         return;
 
     searchAndUpdate(searchBar.value);
+});
+
+// Lucky button gives the first element of the search
+luckyButton.addEventListener("click", e => {
+    e.preventDefault();
+
+    const words = searchBar.value;
+
+    // Won't process empty search query
+    if (words == "undefined")
+        return;
+
+    // Repetitive, should be separated in a function, will be improved in later version
+    const url = "http://localhost:"
+    const port = 3000;
+    const path = "search"
+
+    // changing the search text to let the user to wait
+    searchBar.value = "Searching...";
+
+    // Builds the full url used for the search
+    const fullUrl = `${url}${port}/${path}/${words}`;
+
+    fetch(fullUrl)
+        .then((response) => response.json())
+        .then(data => window.location.replace(data[0].link));
 });
